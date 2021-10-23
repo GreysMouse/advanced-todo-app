db532b7941ee02cf4b62562bcc82bfda56121d82
Diana Kondakova <greys.mouse@yandex.ru>
2021-10-23 18:51:41 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles/app.css';
+import './styles/app__container.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='app__container' />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config.ts
@@ -0,0 +1,15 @@
+export const NOTE_MIN_LENGTH = 1;
+export const NOTE_MAX_LENGTH = 96;
+
+export const NOTE_PLACEHOLDER = 'Type your TODO here';
+export const NOTE_TEXTAREA_MAX_ROWS = 10;
+
+export const TASK_CARD_SELECTION_DELAY = 1000;
+
+export enum DEVICE_TYPES {
+  MOBILE = 'mobile',
+  DESKTOP = 'desktop',
+  LAPTOP = 'laptop'
+}
+
+export const TASKS_BASE_URL = 'http://localhost:4000';
